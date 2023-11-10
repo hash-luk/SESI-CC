0a1bd4b72e183738a22f9f24c9f3027972e8d059
hash-luk <lucas.patrick2506@gmail.com>
2023-11-10 10:55:29 -0300

    feat: Created other options to display input text

--- src/app/formulario/formulario.page.ts
+++ src/app/formulario/formulario.page.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario.page.scss'],
 })
 export class FormularioPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {}
+
+  public showOthersInput(event: any): void {
+    const otherOptionsInput = document.querySelector(".other-options-input");
+    console.log(event)
+
+    if(event.detail.checked) {
+      otherOptionsInput?.setAttribute("style", "display: block;")
+    } else {
+      otherOptionsInput?.setAttribute("style", "display: none;")
+    }
+  }
 }
